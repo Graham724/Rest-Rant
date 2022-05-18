e88d887a81e4c35e18a11a42e394a3006348f6f3
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-17 19:38:20 -0500

    adding styles to rest-rant

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,8 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossOrigin="anonymous"/>
+                <link rel="stylesheet" href="/style.css" />
             </head>
             <body>
                 {html.children}

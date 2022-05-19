09172cb6d9ec230212c1f5ae0f3e8bf11693a53b
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-18 20:33:49 -0500

    can add new places

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,26 +1,9 @@
 const router = require('express').Router()
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/res-tables.jpg'
-    // https://unsplash.com/photos/YYZU0Lo1uXE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee.jpg'
-    // https://unsplash.com/photos/zUNs99PGDg0?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
-  }]
-
-
+const places = require('../models/places.js')
 
 
 router.get('/', (req, res) => {
-      
-    res.render('places/index', { places })
+      res.render('places/index', { places })
 })
 
 
@@ -29,14 +12,24 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-// CREATE
+
 router.post('/', (req, res) => {
-    // if (!req.body.image) {
-    //   req.body.image = '/images/chia-fruit-drink.jpg'
-    // }
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
     places.push(req.body)
     res.redirect('/places')
   })
+  
+  
 
 router.get('/show', (req, res) => {
     res.render('places/show')

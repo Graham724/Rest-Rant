9dc6476dcb779db05cd0694a92038b6c1b9f2c45
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-18 19:46:01 -0500

    jazzed up form page

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,30 +1,42 @@
 const router = require('express').Router()
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/res-tables.jpg'
+    // https://unsplash.com/photos/YYZU0Lo1uXE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee.jpg'
+    // https://unsplash.com/photos/zUNs99PGDg0?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+  }]
+
 
 
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/res-tables.jpg'
-        // https://unsplash.com/photos/YYZU0Lo1uXE?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee.jpg'
-        // https://unsplash.com/photos/zUNs99PGDg0?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
-      }]
       
     res.render('places/index', { places })
 })
 
+
+
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
+
+// CREATE
+router.post('/', (req, res) => {
+    // if (!req.body.image) {
+    //   req.body.image = '/images/chia-fruit-drink.jpg'
+    // }
+    places.push(req.body)
+    res.redirect('/places')
+  })
 
 router.get('/show', (req, res) => {
     res.render('places/show')

98cf05efeb4312ccb5105395457e81f0d7ba2e44
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-05-20 20:07:48 -0500

    edit page almost working

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,22 +1,42 @@
-const router = require('express').Router()
+const router = require ('express').Router()
 const places = require('../models/places.js')
 
+router.get('/', (req, res) => { 
+    res.render('places/index', { places })
+  })
 
-router.get('/', (req, res) => {
-      res.render('places/index', { places })
-})
-
-
-
-router.get('/new', (req, res) => {
+  router.get('/new', (req,res) => {
     res.render('places/new')
-})
+  })
 
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id], id })
+    }
+  })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id], id })
+    }
+  })
+
+  router.post('/', (req, res) => {
+    if (!req.body.pic){
       req.body.pic = 'http://placekitten.com/400/400'
     }
     if (!req.body.city) {
@@ -28,53 +48,42 @@ router.post('/', (req, res) => {
     places.push(req.body)
     res.redirect('/places')
   })
-  
-  
 
-
-//Show
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
-
-//DELETE
-router.delete('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    places.splice(id, 1)
-    res.redirect('/places')
-  }
-})
-
-//EDIT
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
       res.render('error404')
-  }
-  else if (!places[id]) {
+    }
+    else if (!places[id]) {
       res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
-
-
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')    
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      if (!req.body.pic) {
+        req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+        req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+        req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+    }
+  })
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,8 +9,8 @@ function show (data) {
                 <div>
                 <img src={data.place.pic} alt={data.place.name} />
                     <h2>Desctiption</h2>
-                    <p>{ data.place.city }, { data.place.state }</p>
-                    <p>{ data.place.cuisines }</p>
+                    <p>Located in { data.place.city }, { data.place.state }</p>
+                    <p>Serving { data.place.cuisines }</p>
                 </div>
                 <div>
                     <h2>Comments</h2>
@@ -22,7 +22,7 @@ function show (data) {
                 </div>
 
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a> 
-
+                
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">Delete</button>
                 </form> 

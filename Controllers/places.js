aa120eba93adb022b1b958bf6b2e5260a20c1386
Graham Lincoln <graham.72495@gmail.com git config --global user.name Graham>
2022-06-08 19:25:28 -0500

    please workkk

--- Controllers/places.js
+++ Controllers/places.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const db = require('../models')
-const places = require('../models/places.js')
+// const places = require('../models/places.js')
 
 //Get
 router.get('/', (req, res) => {     
